68eccbb0e4819d287fbdaecf70980ff415656763
Paulo Vieira <paullocostta86@gmail.com>
2021-05-03 19:27:16 -0300

    feat: challenge 05 finished

--- index.js
+++ index.js
@@ -7,24 +7,40 @@ const database = {
   createTable(statement) {
     const regexp = /^create\stable\s([a-z]+)\s\((.+)\)/i;
     const parsedStatement = statement.match(regexp);
-    const tableName = parsedStatement[1];
+    let [, tableName, columns] = parsedStatement;
     this.tables[tableName] = {
       columns: {},
       data: []
     }
-    let columns = parsedStatement[2];
     columns = columns.split(",");
     for (let column of columns) {
       column = column.trim().split(" ");
-      const name = column[0];
-      const type = column[1];
+      const [name, type] = column;
       this.tables[tableName].columns[name] = type;
     }
   },
+  insert(statement) {
+    const regexp = /^insert\sinto\s([a-z]+)\s\((.+)\)\svalues\s\((.+)\)/i;
+    const parsedStatement = statement.match(regexp);
+    let [, tableName, columns, values] = parsedStatement;
+    columns = columns.split(", ");
+    values = values.split(", ");
+    let row = {};
+    for (let i = 0; i < columns.length; i++) {
+      const column = columns[i];
+      const value = values[i];
+      row[column] = value;
+    }
+    this.tables[tableName].data.push(row);
+  },
   execute(statement) {
-    const regexp = /^create\stable/i;
-    if (regexp.test(statement)) {
+    const regexpCreate = /^create\stable/i;
+    if (regexpCreate.test(statement)) {
       return this.createTable(statement);
+    }
+    const regexpInsert = /^insert\sinto/i;
+    if (regexpInsert.test(statement)) {
+      return this.insert(statement);
     }
     const message = `Syntax error: "${statement}"`;
     throw new DatabaseError(statement, message);
@@ -32,8 +48,10 @@ const database = {
 };
 try {
   database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+  database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+  database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+  database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
   console.log(JSON.stringify(database, undefined, "  "));
-  database.execute("select id, name from author");
 } catch (err) {
   console.error(err.message);
 }
